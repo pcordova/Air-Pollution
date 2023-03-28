5aeab9774f5a2e17ad0ff0563e51a174e7e59c3e
Pedro Córdova <pjcordova@gmail.com>
2023-03-28 05:31:54 -0300

    added code

--- raw.R
+++ raw.R
@@ -3,7 +3,9 @@ if(!dir.exists("Data")) {dir.create("Data")}
 download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
               "./Data/stormData.bz2", method = "curl")
 
-stormData <- data.table::fread("./Data/stormData.bz2",
+library(data.table)
+
+stormData <- fread("./Data/stormData.bz2",
                                select = c(7,8,23:28),
                                col.names = c("State",
                                              "Event",
@@ -37,7 +39,7 @@ unique(stormData$propUnits)
 
 unique(stormData$cropUnits)
 
-
+aggregate(cbind(stormData$Fatalities, stormData$Injuries), list(stormData$Event), sum)
 
 
 
