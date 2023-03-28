ff57b7c91a82e1f10b5d92d90ff3ffff9d39760d
Pedro Córdova <pjcordova@gmail.com>
2023-03-28 03:24:30 -0300

    added data

--- raw.R
+++ raw.R
@@ -3,5 +3,60 @@ if(!dir.exists("Data")) {dir.create("Data")}
 download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
               "./Data/stormData.bz2", method = "curl")
 
-stormData <- data.table::fread("./Data/stormData.bz2")
+stormData <- data.table::fread("./Data/stormData.bz2",
+                               select = c(7,8,23:28),
+                               col.names = c("State",
+                                             "Event",
+                                             "Fatalities",
+                                             "Injuries",
+                                             "propDamage",
+                                             "propUnits",
+                                             "cropDamage",
+                                             "cropUnits"))
 
+str(stormData)
+
+cat(sum(is.na(stormData)))
+
+unique(stormData$Fatalities)
+
+unique(stormData$Injuries)
+
+
+
+
+
+
+
+
+
+
+cat(NROW(unique(stormData$Event)))
+
+unique(stormData$propUnits)
+
+unique(stormData$cropUnits)
+
+
+
+
+
+########################################################################
+## include description and justification for any data transformations ##
+########################################################################
+
+## figure(s) must have descriptive captions
+
+## which types of events (EVTYPE) are most harmful to population health?
+
+
+
+
+
+## which types of events have the greatest economic consequences?
+
+
+
+
+
+## 
